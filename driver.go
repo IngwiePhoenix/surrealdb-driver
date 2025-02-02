2641017427d2e75cb554ce1470cc86dbcbc1403b
senpro-ingwersenk <kevin.ingwersen@senpro.it>
2025-02-02 03:12:08 +0100

    WIP: Implement more methods

--- driver.go
+++ driver.go
@@ -3,8 +3,8 @@ package surrealdbdriver
 import (
 	"context"
 	"database/sql/driver"
+	"errors"
 	"log/slog"
-	"net/http"
 
 	"github.com/gorilla/websocket"
 )
@@ -12,6 +12,26 @@ import (
 // implements driver.Driver
 type SurrealDriver struct{}
 
+func (d *SurrealDriver) Open(address string) (*SurrealConn, error) {
+	config, err := ParseUrl(address)
+	if err != nil {
+		return nil, err
+	}
+	dialer := websocket.Dialer{}
+	conn, resp, err := dialer.Dial(config.GetDBUrl(), nil)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != 200 {
+		return nil, errors.New("SurrealDB's initial response was not 200: " + resp.Status)
+	}
+	return &SurrealConn{
+		WSClient: conn,
+		Driver:   d,
+	}, nil
+}
+
+// implements driver.DriverContext
 func (*SurrealDriver) OpenConnector(address string) (*SurrealConnector, error) {
 	config, err := ParseUrl(address)
 	if err != nil {
@@ -22,11 +42,6 @@ func (*SurrealDriver) OpenConnector(address string) (*SurrealConnector, error) {
 	}, nil
 }
 
-// implements driver.DriverContext
-type SurrealDriverContext struct{}
-
-func (*SurrealDriverContext) OpenConnector(name string) (SurrealConnector, error)
-
 // implements driver.Connector
 type SurrealConnector struct {
 	Creds *CredentialConfig
@@ -36,15 +51,14 @@ func (*SurrealConnector) Connect(ctx context.Context) (SurrealConn, error)
 
 // implements driver.Conn
 type SurrealConn struct {
-	HTTPClient *http.Client
-	WSClient   *websocket.Conn
-	Driver     *SurrealDriver
-	Logger     *slog.Logger
+	WSClient *websocket.Conn
+	Driver   *SurrealDriver
+	Logger   *slog.Logger
 }
 
 func (*SurrealConn) Prepare(query string) (SurrealStmt, error)
 func (con *SurrealConn) Close() error {
-	con.wsClient.Close()
+	con.WSClient.Close()
 }
 func (*SurrealConn) Begin() (driver.Tx, error)
 func (con *SurrealConn) IsValid() bool {
@@ -55,8 +69,24 @@ func (con *SurrealConn) IsValid() bool {
 }
 
 // implements driver.Validator's method IsValid
-func (*SurrealConn) Exec(sql string, values []driver.Value) (driver.Result, error) {
-	// Write to WS client
+func (con *SurrealConn) Exec(sql string, values []driver.Value) (driver.Result, error) {
+	caller := MakeCaller()
+	mappedValues := map[string]interface{}{}
+	for key, v := range values {
+		mappedValues[string(key)] = v
+	}
+	req := caller.CallQuery(sql, mappedValues)
+	if err := con.WSClient.WriteJSON(req); err != nil {
+		return nil, err
+	}
+	res := &SurrealAPIResponse{}
+	if err := con.WSClient.ReadJSON(res); err != nil {
+		return nil, err
+	}
+
+	return &SurrealResult{
+		RawResult: res,
+	}, nil
 }
 
 // implements driver.Stmt
@@ -78,3 +108,29 @@ func (*SurrealRows) Next(dest []driver.Value) error
 
 // Implement
 type SurrealScanner struct{}
+
+type SurrealResult struct {
+	RawResult *SurrealAPIResponse
+}
+
+// SurrealDB's "record IDs" are strings, not ints.
+// this is likely going to be a problem and a half...
+// I WISH there was a way to represent a record ID nummerically.
+func (r *SurrealResult) LastInsertId() (int64, error) {
+	if r.RawResult.Error != nil {
+		return 0, errors.New(r.RawResult.Error.Message)
+	}
+	return 0, nil
+}
+func (r *SurrealResult) RowsAffected() (int64, error) {
+	if value, ok := r.RawResult.Result.([]interface{}); ok {
+		return int64(len(value)), nil
+	}
+	if _, ok := r.RawResult.Result.(interface{}); ok {
+		return 1, nil
+	}
+	if r.RawResult.Error != nil {
+		return 0, errors.New(r.RawResult.Error.Message)
+	}
+	return 0, errors.New("RowsAffected() fell through")
+}
