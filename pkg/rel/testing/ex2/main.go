39f7d4822fa17aa3d6ade164062f974d21800bf3
Ingwie Phoenix <ingwie@birb.it>
2025-02-13 22:20:09 +0100

    rel: Start to make it a proper define, create, insert, update, delete example

--- pkg/rel/testing/ex2/main.go
+++ pkg/rel/testing/ex2/main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"context"
-	"fmt"
 	"log"
 	"time"
 
 	driver "github.com/IngwiePhoenix/surrealdb-driver"
 	srel "github.com/IngwiePhoenix/surrealdb-driver/pkg/rel"
-	"github.com/go-rel/rel"
 )
 
 type Entity struct {
@@ -56,6 +54,7 @@ type Task struct {
 }
 
 type Process struct {
+	ID          string    `db:"id"`
 	Title       string    `db:"title"`
 	Description string    `db:"description"`
 	CreatedAt   time.Time `db:"created_at"`
@@ -79,6 +78,9 @@ func main() {
 	// enable logging on the driver
 	driver.SurrealDBDriver.SetLogger(log.Default())
 
+	// context
+	ctx := context.Background()
+
 	adapter, err := srel.Open("ws://root:root@127.0.0.1:8000/rpc?method=root&db=dsbt&ns=dsbt")
 	if err != nil {
 		log.Println("Could not make adapter")
@@ -87,7 +89,9 @@ func main() {
 	defer adapter.Close()
 	//srAdapter := adapter.(*srel.SurrealDB)
 
-	repo := srel.NewRepo(adapter)
+	repo := srel.NewRepo(adapter) // Using SurrealDB adapter
+
+	// Logging
 	repo.Instrumentation(func(ctx context.Context, op, message string, args ...interface{}) func(err error) {
 		log.Printf("[LOG] (%s) %s : %v\n", op, message, args)
 		return func(err error) {
@@ -98,11 +102,52 @@ func main() {
 		}
 	})
 
-	var process []Process
-	repo.MustFindAll(
-		context.Background(),
-		&process,
-		rel.From("process"),
-	)
-	fmt.Println(process)
+	// Write schemas if they do not exist
+	sql := `
+		DEFINE NAMESPACE IF NOT EXISTS dsbt;
+		USE NS dsbt;
+		DEFINE DATABASE IF NOT EXISTS dsbt;
+		USE DB dsbt;
+		DEFINE TABLE IF NOT EXISTS processes SCHEMAFULL;
+		DEFINE FIELD IF NOT EXISTS title ON processes TYPE string;
+		DEFINE FIELD IF NOT EXISTS description ON processes TYPE string;
+		DEFINE FIELD IF NOT EXISTS created_at ON processes TYPE datetime;
+		DEFINE FIELD IF NOT EXISTS created_at ON processes TYPE datetime;
+		DEFINE FIELD IF NOT EXISTS responsible ON processes TYPE array<string>;
+		DEFINE FIELD IF NOT EXISTS legal_basis ON processes TYPE array<string>;
+		DEFINE FIELD IF NOT EXISTS risks ON processes TYPE array<string>;
+		DEFINE FIELD IF NOT EXISTS storage ON processes TYPE array<string>;
+		DEFINE FIELD IF NOT EXISTS tasks ON processes TYPE array<string>;
+		DEFINE FIELD IF NOT EXISTS affected_data ON processes TYPE array<string>;
+	`
+	affected, lastidx := repo.MustExec(ctx, sql)
+	log.Println("Ran query", affected, lastidx)
+
+	// Let's make an empty process and try to insert.
+	lohnabrechnung := Process{
+		ID:           "processes:lohnabrechnung",
+		Title:        "Lohnabrechnung",
+		Description:  "Beispiel der Lohnabrechnung",
+		CreatedAt:    time.Now(),
+		UpdatedAt:    time.Now(),
+		Responsible:  []string{"Alex", "Daniel"},
+		LegalBasis:   []string{"GOB", "Wirtschaft"},
+		Risks:        []string{"Datendiebstahl", "Ausnutzung"},
+		Storage:      []string{"NAS", "Aktenschrank"},
+		Tasks:        []string{"Einholen der Bankdaten"},
+		AffectedData: []string{"Kontoinformationen"},
+	}
+
+	repo.MustInsert(ctx, &lohnabrechnung)
+	log.Println("Inserted data")
+
+	/*
+		var process []Process
+		repo.MustFindAll(
+			context.Background(),
+			&process,
+			rel.From("process"),
+		)
+		fmt.Println(process)
+	*/
 }
